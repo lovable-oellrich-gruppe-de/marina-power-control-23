2c940d927fde5d58d70019d42877247ecbf75b59
wießnicht <lovable@oellrich-gruppe.de>
2025-05-06 15:54:18 +0200

    Update users.php

--- users.php
+++ users.php
@@ -160,10 +160,10 @@ function sortLink($label, $column) {
                                                 </svg>
                                             </a>
                                             <a href="user_form.php?id=<?= $user['id'] ?>" class="text-marina-600 hover:text-marina-900" title="Bearbeiten">
-                                              <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                                                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                                              </svg>
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                                  <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                                                  <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                                                </svg>
                                             </a>
                                             <?php if ($user['id'] !== $_SESSION['user_id']): ?>
                                                 <a href="#" onclick="confirmDeleteUser('<?= $user['id'] ?>', '<?= htmlspecialchars(addslashes($user['name'])) ?>')" class="text-red-600 hover:text-red-900 p-1" title="Löschen">
