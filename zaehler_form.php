f7c6c970587851133a7c54efc67dc214a95844e3
wießnicht <lovable@oellrich-gruppe.de>
2025-05-08 13:42:57 +0200

    Update zaehler_form.php

--- zaehler_form.php
+++ zaehler_form.php
@@ -192,7 +192,7 @@ require_once 'includes/header.php';
 
                     <div class="space-y-2">
                         <label for="zaehlernummer" class="block text-sm font-medium text-gray-700">Zählernummer *</label>
-                        <input type="text" id="zaehlernummer" name="zaehlernummer" value="<?= htmlspecialchars($zaehler['zaehlernummer']) ?>" <!--required-->
+                        <input type="text" id="zaehlernummer" name="zaehlernummer" value="<?= htmlspecialchars($zaehler['zaehlernummer']) ?>" required
                                class="flex h-10 w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-base focus:outline-none focus:ring-2 focus:ring-marina-500 focus:border-marina-500">
                     </div>
                     
